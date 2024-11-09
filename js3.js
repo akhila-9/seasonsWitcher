33ca83fcae81c6b64b45d9d36911aa81a0f28995
akhilagit09 <120798830+akhilagit09@users.noreply.github.com>
2024-11-09 22:23:25 +0530

    third commit

---
+++ js3.js
@@ -0,0 +1,28 @@
+let smallimage=document.getElementById("fourSeasons");
+let mdimage=document.getElementById("fourSeasons-1");
+let springsmall ="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+let springmedium="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png";
+let summersmall="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png";
+let summermedium="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png";
+let autumnsmall="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png";
+let autumnmedium="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png";
+let wintersmall="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png";
+let wintermedium="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png";
+
+
+function spring(){
+    smallimage.src=springsmall;
+    
+}
+function summer(){
+   smallimage.src=summersmall;
+
+}
+function autumn(){
+    smallimage.src=autumnsmall;
+}
+function winter(){
+    smallimage.src=wintersmall;
+    
+
+}
